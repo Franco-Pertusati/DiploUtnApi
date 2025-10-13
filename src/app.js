b5eb03db1f31e89e3f1efaaad72e6b31447600f3
Franco Pertusati <pertusatifranco836@gmail.com>
2025-10-13 11:00:46 -0300

    Configuracion inicial con documentacion

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const OpenApiValidator = require("express-openapi-validator");
+const routes = require("./routes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const YAML = require("yamljs");
+swaggerDocument = YAML.load("./openapi.yaml");
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true,
+    validateResponses: true,
+    ignorePaths: /.*\/docs.*/,
+  })
+);
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use("/", routes);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+module.exports = app;
